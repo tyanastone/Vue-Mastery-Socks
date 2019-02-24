118bf39d870adeafcadf287f8d8b6a1380353c15
Tyana Stone <tyanastone@Moniques-MacBook.local>
2019-02-24 18:32:13 -0500

    add css to color switch

--- main.js
+++ main.js
@@ -3,6 +3,28 @@ var app = new Vue({
     data: {
         product: 'Socks',
         image: './images/vmSocks-green-onWhite.jpg',
-        
+        inStock: false,
+        details: ["80% cotton", "20% polyester", "Gender-neutral"],
+        variants: [
+            {
+              variantId: 2234,
+                variantColor: 'green',
+              variantImage: './images/vmSocks-green-onWhite.jpg'
+            },
+            {
+              variantId: 2235,
+                variantColor: 'blue',
+              variantImage: './images/vmSocks-blue-onWhite.jpg'
+            }
+        ],
+        cart: 0
+    },
+    methods: {
+        addToCart () {
+            this.cart += 1
+        },
+        updateProduct(variantImage) {
+            this.image = variantImage
+        }
     }
 })
